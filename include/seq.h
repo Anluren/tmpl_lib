768ac0baa91771999ac78248ee075c238d18dc11
Dengfeng Zheng <zhengdengfeng@gmail.com>
2024-04-20 17:30:40 -0400

    add make_one_based_index_sequence

--- include/seq.h
+++ include/seq.h
@@ -1,10 +1,11 @@
+#include <__utility/integer_sequence.h>
 #include <cstddef>
 #include <type_traits>
 #include <utility>
 #include <string>
 
-template<int... Is>
-std::string sequence_to_string(std::integer_sequence<int, Is...>) {
+template<typename T, T... Is>
+std::string sequence_to_string(std::integer_sequence<T, Is...>) {
     std::string result;
     ((result += std::to_string(Is) + ' '), ...);
     return result;
@@ -23,10 +24,27 @@ auto gen_total_value_sequence_impl(const std::index_sequence<Is...>, const std::
         <int, gen_total_valuee<Is>(std::integer_sequence<int, Vs...>{})...>{};
 }
 
-
-#include <utility> // Add missing include directive
-
 template<int ... Vs>
 auto gen_total_value_sequence(const std::integer_sequence<int, Vs...>& seq) {
     return gen_total_value_sequence_impl(std::make_index_sequence<sizeof...(Vs)>{}, seq);
+}
+
+template<std::size_t Min, std::size_t Max, std::size_t... Is>
+auto make_index_sequence_range_impl(std::index_sequence<Is...>) {
+    return std::index_sequence<(Min + Is)...>{};
+}
+
+template<std::size_t Min, std::size_t Max>
+auto make_index_sequence_range() {
+    return make_index_sequence_range_impl<Min, Max>(std::make_index_sequence<Max - Min>{});
+}
+
+// make 1 based index sequence, and last element is 0
+template <std::size_t N, std::size_t ...Is>
+auto make_one_based_index_sequence_impl(std::index_sequence<Is...>) {
+    return std::integer_sequence<std::size_t, ((Is + 1)%N)...>{};
+}
+template<std::size_t N>
+auto make_one_based_index_sequence() {
+    return make_one_based_index_sequence_impl<N>(std::make_index_sequence<N>{});
 }
--- test/test.cpp
+++ test/test.cpp
@@ -9,3 +9,13 @@ TEST(SeqTest, SumSequence) {
 
     EXPECT_EQ(sequence_to_string(sum_seq), "0 1 3 6 10 ");
 }
+
+TEST(SeqTest, MakeIndexSequenceRange) {
+    auto seq = make_index_sequence_range<1, 5>();
+    EXPECT_EQ(sequence_to_string(seq), "1 2 3 4 ");
+    }
+
+TEST(SeqTest, MakeOneBasedIndexSequence) {
+        auto seq = make_one_based_index_sequence<5>();
+        EXPECT_EQ(sequence_to_string(seq), "1 2 3 4 0 ");
+}
